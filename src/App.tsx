4a0bda696cd8ee9d3cd997ddf74bcfdf5b5067a4
h3ct0r <isc.quch@gmail.com>
2023-09-01 23:46:06 -0500

    app setup with mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import CssBaseline from '@mui/material/CssBaseline'
+import {ThemeProvider, createTheme} from '@mui/material/styles'
+
+const defaultTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      Sundaes on Demand!
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App

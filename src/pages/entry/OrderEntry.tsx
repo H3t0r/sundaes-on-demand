5318b5e55f7f081c4fbb2f6ec2be95703cc6ea2c
h3ct0r <isc.quch@gmail.com>
2023-09-15 20:44:24 -0500

    adjust order entry design

--- src/pages/entry/OrderEntry.tsx
+++ src/pages/entry/OrderEntry.tsx
@@ -1,9 +1,10 @@
-import Typography from '@mui/material/Typography'
-import Options from './Options'
-import {useOrderDetails} from '../../context/OrderDetails'
-import {formatCurrency} from '../../utilities'
 import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import {useOrderDetails} from '../../context/OrderDetails'
 import {OrderPhase} from '../../types'
+import {formatCurrency} from '../../utilities'
+import Options from './Options'
 
 interface Props {
   onConfirm: React.Dispatch<React.SetStateAction<OrderPhase>>
@@ -14,15 +15,21 @@ function OrderEntry(props: Props) {
 
   return (
     <>
-      <Typography variant="h2">Design Your Sundae!</Typography>
-      <Options type="scoops" />
-      <Options type="toppings" />
-      <Typography variant="h2">
+      <Typography variant="h2" mb={2}>
+        Design Your Sundae!
+      </Typography>
+      <Stack mb={3} spacing={2}>
+        <Options type="scoops" />
+        <Options type="toppings" />
+      </Stack>
+      <Typography variant="h3" mb={2}>
         Grand total: {formatCurrency(totals.scoops + totals.toppings)}
       </Typography>
       <Button
+        variant="contained"
         disabled={totals.scoops <= 0}
         onClick={() => props.onConfirm('review')}
+        size="large"
       >
         Order Sundae!
       </Button>
